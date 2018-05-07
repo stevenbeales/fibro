5932390219c80390e5748be56cad6efaf7b15b17
Steven Beales <stevenbeales@gmail.com>
2018-05-07 17:12:32 -0400

    Add error logging

--- app.rb
+++ app.rb
@@ -14,24 +14,38 @@ require 'sinatra/activerecord'
 require 'sinatra-initializers'
 require 'ralyxa'
 require_relative 'config/db'
-require './app/app_constants'
-require 'json'
+require_relative 'app/app_constants'
 require_relative 'app/models/init'
 require_relative 'helpers'
 
 # Main App Class and Entry Point
 class App < Sinatra::Base
-  PADDING_LEN = 25 # for the type: "SSML" and ssml: parts
-  OPENING_TAG = "<speak>"
-  CLOSING_TAG = "</speak>"
-  MAX_RESPONSE_LEN = 8000 # Give extra characters for conversion to json
+  include AppConstants
+  enable :sessions
+  enable :logging
 
   configure { set :server, :puma }
   set :app_file, __FILE__
   set :root, File.dirname(__FILE__)
   set :public_folder, File.dirname(__FILE__) + '/public'
-  set :log_file, File.dirname(__FILE__) + AppConstants::LOG_FILE
-  enable :sessions
+  set :log_file, File.dirname(__FILE__) + LOG_FILE
+  set :error_log_file, File.dirname(__FILE__) + ERROR_LOG_FILE
+
+  # Rack request logging
+  configure do
+    file = File.new(settings.log_file, 'a+')
+    # ensure file flushes to disk automatically
+    file.sync = true
+    use Rack::CommonLogger, file
+  end
+
+  # Rack and custom error logging
+  error_log = File.new(settings.error_log_file, 'a+')
+  error_log.sync = true
+  # ensure file flushes to disk automatically
+  before do
+    env["rack.errors"] = error_log
+  end
 
   # Register initializers a la Rails
   register Sinatra::Initializers
@@ -47,7 +61,7 @@ class App < Sinatra::Base
   # Entry point for requests from Amazon Alexa.
   # The incoming requests are dispatched to intents in the intents folder by Ralyxa.
   post '/' do
-    puts "Received request with headers:\n#{request.env}"
+    error_log.write("Received request with headers:\n#{request.env}")
 
     begin
       Ralyxa::Skill.handle(request)
@@ -62,7 +76,7 @@ class App < Sinatra::Base
 
   post '/rate-pain' do
     req_body = request.body.read
-    puts "REQUEST BODY: #{req_body}"
+    error_log.write("REQUEST BODY: #{req_body}")
     req_params = JSON.parse req_body
 
     session = RatePain.get_session req_params
@@ -72,7 +86,7 @@ class App < Sinatra::Base
 
   # For debugging
   get '/rate-pain' do
-    puts "Received request with headers:\n#{request.env}"
+    error_log.write("Received request with headers:\n#{request.env}")
     rate_pain_session = RatePainSession.new
     resp_text = rate_pain_session.rate_pain
     make_ssml_response resp_text, false
--- app/app_constants.rb
+++ app/app_constants.rb
@@ -3,5 +3,10 @@
 # This module contains all application constants
 module AppConstants
   APP_NAME = 'Fibro Friend'
+  CLOSING_TAG = "</speak>"
+  ERROR_LOG_FILE = './log/error.log'
   LOG_FILE = './log/fibro.log'
+  MAX_RESPONSE_LEN = 8000 # Give extra characters for conversion to json
+  OPENING_TAG = "<speak>"
+  PADDING_LEN = 25 # for the type: "SSML" and ssml: parts
 end
