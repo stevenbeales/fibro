3ec49ce1f63f3069e19c595f581d1c1da7eee963
Steven Beales <stevenbeales@gmail.com>
2018-05-07 21:41:01 -0400

    Touch log files during script to fix travis build

--- app/app_constants.rb
+++ app/app_constants.rb
@@ -4,8 +4,8 @@
 module AppConstants
   APP_NAME = 'Fibro Friend'
   CLOSING_TAG = "</speak>"
-  ERROR_LOG_FILE = './log/error.log'
-  LOG_FILE = './log/fibro.log'
+  ERROR_LOG_FILE = '/log/error.log'
+  LOG_FILE = '/log/fibro.log'
   MAX_RESPONSE_LEN = 8000 # Give extra characters for conversion to json
   OPENING_TAG = "<speak>"
   PADDING_LEN = 25 # for the type: "SSML" and ssml: parts
